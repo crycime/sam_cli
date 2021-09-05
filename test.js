7ac71aaaa3d87b87cead29f2528c2835530ddbf6
zizhenli <zizhenli@hk01.com>
2021-09-05 21:30:00 +0800

    no message

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-describe('getEnumNumericValues', () => {
-	it('should return an array of values that is numeric from the enum provided', () => {
-		expect(1+1).toEqual(2)
+describe('match', () => {
+	it('sum', () => {
+		expect(1+2).toEqual(3)
 	})
 })
