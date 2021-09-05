196c1209e8ab2122bda6adf878f3043bc3559f4c
zizhenli <zizhenli@hk01.com>
2021-09-05 23:42:29 +0800

    no message

--- test.js
+++ test.js
@@ -1,16 +1,5 @@
 describe('match', () => {
 	it('sum', async () => {
-		await time(4)
-		expect(1+2).toEqual(3)
-	})
-
-	it('sum', async () => {
-		await time(4)
-		expect(1+2).toEqual(3)
-	})
-
-	it('sum', async () => {
-		await time(4)
 		expect(1+2).toEqual(3)
 	})
 })
