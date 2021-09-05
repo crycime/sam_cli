ffb1a14971c6c7f9086920a69ebd05c1c57a234f
crycime <36436659+crycime@users.noreply.github.com>
2021-09-05 21:52:20 +0800

    no message (#2)
    Co-authored-by: zizhenli <zizhenli@hk01.com>

--- test.js
+++ test.js
@@ -1,5 +1,25 @@
 describe('match', () => {
-	it('sum', () => {
+	it('sum', async () => {
+		await time(4)
+		expect(1+2).toEqual(3)
+	})
+
+	it('sum', async () => {
+		await time(4)
+		expect(1+2).toEqual(3)
+	})
+
+	it('sum', async () => {
+		await time(4)
 		expect(1+2).toEqual(3)
 	})
 })
+
+
+const time = second => {
+	return new Promise(function(resolve, reject) {
+		setTimeout(function() {
+			resolve(5)
+		}, second * 1000)
+	})
+}
